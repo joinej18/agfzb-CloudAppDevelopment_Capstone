4de24151355c35978b7fb8f6bf81282ae6b75e11
joinej18 <joinej18.me@gmail.com>
2022-11-10 16:24:42 -0500

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -80,6 +80,7 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    context = {get_dealers_from_cf(url)}
     if request.method == "GET":
         url = "https://us-south.functions.appdomain.cloud/api/v1/web/JJProjects_Cloud%20Projects/dealership-package/get-dealerships "
         # Get dealers from the URL
@@ -87,14 +88,15 @@ def get_dealerships(request):
         # Concat all dealer's short name
         dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
         # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-#def get_dealer_details(request, dealer_id):
-    #print review_obj.sentiment
-    #reviews = get_dealer_reviews_from_cf(dealer_id)
-    #return HttpResponse(dealer_id)
+def get_dealer_details(request, dealer_id):
+    context = {get_dealer_reviews_from_cf(dealer_id)}
+    print review_obj.sentiment
+    reviews = get_dealer_reviews_from_cf(dealer_id)
+    return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
-#def add_review(request, dealer_id):
+def add_review(request, dealer_id):

7fd976128da6bdbd1e65cffe90cd554a416c171f
joinej18 <106044374+joinej18@users.noreply.github.com>
2022-11-15 19:34:21 -0600

    Update urls.py
    updating login and logout request

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,9 +16,9 @@ urlpatterns = [
     # path for registration
     path(route="registration/", view=views.registration_request, name='registration'),
     # path for login
-    path(route="login_request/", view=views.login_request, name='login_request'), 
+    path(route="login/", view=views.login_request, name='login'), 
     # path for logout
-    path(route="logout_request/", view=views.logout_request, name='logout_request'),
+    path(route="logout/", view=views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
